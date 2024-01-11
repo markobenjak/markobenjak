ba2c4b8645dbafc6984a03f9339419ed351eaf41
mbenjak <marko.benjak@dhimahi.com>
2024-01-11 11:16:06 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <body>
+    <main>
+      <div class="h-100 d-flex align-items-center justify-content-center">
+        WebPage Loading...
+      </div>
+      <div class="h-100 d-flex align-items-center justify-content-center">
+        email: marko.benjak@gmail.com
+      </div>
+
+    </main>
+  </body>
+</template>
+
+<script>
+export default {
+    setup () {
+    },
+    data() {
+     
+    },
+    validations () {
+     
+    },
+    methods: {
+     
+    },
+    
+}
+</script>
+
+<style scoped>
+
+.container-md{
+  border:1px solid;
+}
+
+</style>
